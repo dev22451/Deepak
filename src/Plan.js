6bec9827c62195122eacd3b131b4288af61cece7
Deepakbsr <Deepak.hangingpanda@gmail.com>
2021-10-21 19:10:00 +0530

    change ui desigin

--- src/Plan.js
+++ src/Plan.js
@@ -7,7 +7,7 @@ import './App.css';
         <> 
        <div className="deep">
            
-        <li className="shadow "><input type="checkbox" className="max"onClick={() => { props.sendData(props.id) }} /> {props.value}<span className="daye" >Complete</span></li>
+        <li className="shadow "><input type="checkbox" className="max" /> {props.value}<span className="daye" >Complete</span></li>
         <button className="sumo">Edit</button>
         <button className="kumar" onClick={() => { props.sendData(props.id) }}>X</button>
        </div> 
